c634ad0520bc5db35892a1f1dce5ae5f86de5caa
AdamMTobey <AdamMTobey@users.noreply.github.com>
2020-05-15 15:26:50 -0400

    ui_update

--- src/pages/Public/Home/index.js
+++ src/pages/Public/Home/index.js
@@ -18,11 +18,12 @@ class Public extends React.Component {
                 width: "100vw",
                 backgroundColor: '#2e2e2e',
                 position: "relative",
-                color: '#efefef'
+                color: '#efefef',
+                // display: 'flex', alignItems: 'center', justifyContent: 'center'
               }}>
-                <Link style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{height: 300, width: '100%', background: 'url(/img/treering.png)', backgroundPosition: 'center',  }}>
-                        <div className='container' style={{height:'100%'}}>
+                <Link to='/treeringviewer' style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', padding: '25px' }}>
+                    <div style={{height: 250, width: '75%', background: 'url(/img/treering.png)', backgroundPosition: 'center', margin: 'auto' }}>
+                        <div className='container' style={{height:'75%'}}>
                             <div className='row' style={{height:'100%'}}>
                                 <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.35)', height: '90%', margin: 10}}>
                                     <h1 style={{color:'#cfcfcf'}}>Tree Ring Viewer</h1>
@@ -34,28 +35,28 @@ class Public extends React.Component {
                         </div>
                     </div>   
                 </Link>
-                <Link to='/treeringviewer' style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{height: 300, width: '100%', background: 'url(/img/foreststress.png)', backgroundPosition: 'center right',  }}>
-                        <div className='container' style={{height:'100%'}}>
+                <Link to='/forest-stress' style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', padding: '25px'}}>
+                    <div style={{height: 250, width: '75%', background: 'url(/img/foreststress3.png)', backgroundPosition: 'center right',  }}>
+                        <div className='container' style={{height:'75%'}}>
                             <div className='row' style={{height:'100%'}}>
-                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.35)', height: '90%', margin: 10}}>
+                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.75)', height: '90%', margin: 10}}>
                                     <h1 style={{color:'#cfcfcf'}}>Forest Stress</h1>
                                     <p style={{color:'#efefef', textDecoration: 'none'}}>
-                                    Tree Ring Viewer: The PIRE CREATE visualization tool provides unique access to NOAA's tree-ring and speleothem archives, as well as an unrivaled number of new, unpublished records, via an interactive mapping and visualization platform for the scientific community, policy-makers and public at large. The visualization platform incorporates PIRE’s collection of paleoclimate research to generate powerful insights for government and public officials of the future risks associated with climate change around the world.
+                                    Researchers at the University at Albany Department of Atmospheric and Environmental Sciences (DAES) have turned to more than a century’s worth of data (from 1901 to 2012) through NOAA’s International Tree Ring Data Bank to analyze historical tree growth at 3,579 forests around the world and create a model for future projections (from 2045 to 2060). The Forest Stress visualization platform depicts this data on an unprecedented, global scale.
                                     </p>
                                 </div>
                             </div>
                         </div>
                     </div>   
                 </Link>
-                <Link to='/phyda' style={{display: 'flex', flexDirection: 'column'}}>
-                    <div style={{height: 300, width: '100%', background: 'url(/img/phyda.png)', backgroundPosition: 'center right',  }}>
-                        <div className='container' style={{height:'100%'}}>
+                <Link to='/phyda' style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', padding: '25px'}}>
+                    <div style={{height: 250, width: '75%', background: 'url(/img/phyda2.png)', backgroundPosition: 'center',  }}>
+                        <div className='container' style={{height:'75%'}}>
                             <div className='row' style={{height:'100%'}}>
                                 <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.35)', height: '90%', margin: 10}}>
                                     <h1 style={{color:'#cfcfcf'}}>PHYDA</h1>
                                     <p style={{color:'#efefef', textDecoration: 'none'}}>
-                                    Tree Ring Viewer: The PIRE CREATE visualization tool provides unique access to NOAA's tree-ring and speleothem archives, as well as an unrivaled number of new, unpublished records, via an interactive mapping and visualization platform for the scientific community, policy-makers and public at large. The visualization platform incorporates PIRE’s collection of paleoclimate research to generate powerful insights for government and public officials of the future risks associated with climate change around the world.
+                                    This Paleo Hydrodynamics Data Assimilation product (PHYDA) visualization tool maps 2,000 years of reconstructed hydroclimate and associated climate dynamical variables onto a global interface. Using annually or seasonally resolved global reconstructions, two spatiotemporal drought indices, near-surface air temperature, This innovative transformation optimizes exploration of an expansive database into an approachable web-based visualization platform for streamlined presentation and collaboration.
                                     </p>
                                 </div>
                             </div>
