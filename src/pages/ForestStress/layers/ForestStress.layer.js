08ddf41120044d3e437636763ded27643d54e195
AdamMTobey <AdamMTobey@users.noreply.github.com>
2020-05-22 12:58:53 -0400

    foreststresstext

--- src/pages/ForestStress/layers/ForestStress.layer.js
+++ src/pages/ForestStress/layers/ForestStress.layer.js
@@ -8,9 +8,17 @@ import styled from "styled-components"
 
 
 let AboutText = {
-  synchrony: 'syncrhony Lorem ipsum',
-  change: 'Lorem Ipsem stuf and stuff',
-  significance: 'significancesignificance significancesignificancesignificance significance'
+  synchrony: `Climate change impacts extend well beyond an increase in temperature or changes in precipitation regimes. Climate change is affecting forest ecosystems around the world. Our forests offer many valuable services. For instance, trees are carbon sinks, capturing CO2 – the main greenhouse gas warming up our planet – from the air and storing it until they die. As climate change continues, the level of environmental stress that these trees are undergoing is changing, and so it is their potential to act as carbon sinks (among other functions).
+How can we study the level of environmental stress of trees?
+There are multiple ways to study this particular issue. In our case, we used more than a century’s worth of data (from 1901 to 2012) from NOAA’s International Tree Ring Data Bank to analyze historical tree growth at 3,579 forests around the world and study how climate is affecting their level of environmental stress. To do this, we assessed climate’s historical impacts, such as changes in precipitation and temperature, on forest tree-ring growth through a concept known as ‘synchrony’, which relies on the assumption that the world around us is a spatially, auto-correlated system. We found that a large portion of this synchrony can be explained by climate (temperature and precipitation).
+`,
+  change: `What does synchrony mean?
+A forest with high synchrony is more environmentally stressed. In other words, the climate stress levels of that forest are higher, and the opposite is true in forests with low synchrony. Both high and low synchrony forests have multiple implications. For instance, a high synchrony forest would have less potential to act as a carbon pool, and a low synchrony forest would be more affected by local characteristics (competition, insect outbreaks, fires, etc..). Synchrony can therefore serve as a tool for diagnosing effects of global climate change on tree growth of global forests.
+`,
+  significance: `Why is this important?
+It is crucial to understand how the synchrony of the different forests around the world is changing with climate to determine potential adaptation/ mitigation measures for specific regions.
+In addition, we use our model to predict the stress levels of the global forests in the current climate and in a future climate scenario (2045-2060). As a result, we are able to detect the potential changes that the global forests will undergo in the near future, giving us a tool to determine potential measures for ecosystem management in specific regions.
+`
 
 }
 
