43d1ac3b51ecb59fe4a16d1ac3cc81630e4a4fa0
Eric Krans <erickrans@gmail.com>
2020-05-25 16:10:00 -0400

    PIRECREATElogo

--- src/pages/Public/Home/index.js
+++ src/pages/Public/Home/index.js
@@ -21,11 +21,55 @@ class Public extends React.Component {
                 color: '#efefef',
                 // display: 'flex', alignItems: 'center', justifyContent: 'center'
               }}>
+                <Link to='/' style={{display: 'flex', padding: '25px' }}>
+                    <div style={{height: 250, width: '75%', margin: 'auto' }}>
+                        <div className='row' style={{height:'100%'}}>
+                         <div className='col-12 col-md-12' style={{background: 'url(/img/pirecreate.png)', height: '95%', margin: 5}}>
+                         </div>
+                        </div>
+                     </div>
+                </Link>
+                <Link to='/' style={{display: 'flex', padding: '25px' }}>
+                     <div style={{width: '75%', margin: 'auto' }}>
+                        <div className='container' style={{height:'100%'}}>
+                            <div className='row' style={{height:'100%'}}>
+                                <div className='col-12 col-md-12' style={{height: '95%', margin: 5}}>
+                                    <h1 style={{color:'#cfcfcf', fontSize: '1.35em'}}>About The PIRE CREATE Project</h1>
+                                    <p style={{color:'#efefef', fontSize: '1.0em'}}>
+                                    
+                                    PIRE (Partnership in International Research and Education) is a
+                                    program funded by international science agencies (in the US it is
+                                    the National Science Foundation), which supports international
+                                    partnerships that address critical science and engineering
+                                    problems in a global context, thereby facilitating the development
+                                    of a diverse, globally-engaged, science and engineering
+                                    workforce.
+                                    
+                                    PIRE - CREATE is an international collaboration that involves six
+                                    institutions and 34 investigators from the United States
+                                    (University at Albany - State University of New York &amp; Columbia
+                                    University), University of Sao Paulo in Brazil and IANIGLA in
+                                    Argentina.
+
+                                    We are working to advance paleoclimate research over the
+                                    Americas to the point where it can transform the way we inform
+                                    policy- and decision-making at a trans-national level about the
+                                    risks of climate change and place future projections in a broad
+                                    historical context. The project is co-funded by the US National
+                                    Science Foundation (NSF) and the Sao Paulo State Research
+                                    Agency (FAPESP).  Learn more about the PIRE CREATE Project﻿.
+                                    </p>
+                                </div>
+                              
+                            </div>
+                        </div>
+                    </div>
+                </Link>
                 <Link to='/treeringviewer' style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column', padding: '25px' }}>
                     <div style={{height: 250, width: '75%', background: 'url(/img/treering.png)', backgroundPosition: 'center', margin: 'auto' }}>
                         <div className='container' style={{height:'100%'}}>
                             <div className='row' style={{height:'100%'}}>
-                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.35)', height: '90%', margin: 10}}>
+                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.55)', height: '95%', margin: 5}}>
                                     <h1 style={{color:'#cfcfcf', fontSize: '1.35em'}}>Tree Ring Viewer</h1>
                                     <p style={{color:'#efefef', fontSize: '1.0em'}}>
                                     Tree Ring Viewer: The PIRE CREATE visualization tool provides unique access to NOAA's tree-ring and speleothem archives, as well as an unrivaled number of new, unpublished records, via an interactive mapping and visualization platform for the scientific community, policy-makers and public at large. The visualization platform incorporates PIRE’s collection of paleoclimate research to generate powerful insights for government and public officials of the future risks associated with climate change around the world.
@@ -39,10 +83,11 @@ class Public extends React.Component {
                     <div style={{height: 250, width: '75%', background: 'url(/img/foreststress5.png)', backgroundPosition: 'center right',  }}>
                         <div className='container' style={{height:'100%'}}>
                             <div className='row' style={{height:'100%'}}>
-                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.75)', height: '90%', margin: 10}}>
+                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.55)', height: '95%', margin: 5}}>
                                     <h1 style={{color:'#cfcfcf', fontSize: '1.35em'}}>Forest Stress</h1>
                                     <p style={{color:'#efefef', textDecoration: 'none', fontSize: '1.0em'}}>
-                                    Researchers at the University at Albany Department of Atmospheric and Environmental Sciences (DAES) have turned to more than a century’s worth of data (from 1901 to 2012) through NOAA’s International Tree Ring Data Bank to analyze historical tree growth at 3,579 forests around the world and create a model for future projections (from 2045 to 2060). The Forest Stress visualization platform depicts this data on an unprecedented, global scale.
+                                    Researchers at the University at Albany Department of Atmospheric and Environmental Sciences (DAES) have turned to more than a century’s worth of data (from 1901 to 2012) through NOAA’s International Tree Ring Data Bank to analyze historical tree growth at 3,579 forests around the world and create a model for future projections (from 2045 to 2060). The Forest Stress visualization platform depicts this data on an unprecedented, global scale. 
+                                    Learn more about this study <a href="https://onlinelibrary.wiley.com/doi/epdf/10.1111/geb.13090" target="_blank">here</a>
                                     </p>
                                 </div>
                             </div>
@@ -53,7 +98,7 @@ class Public extends React.Component {
                     <div style={{height: 250, width: '75%', background: 'url(/img/phyda2.png)', backgroundPosition: 'center',  }}>
                         <div className='container' style={{height:'100%'}}>
                             <div className='row' style={{height:'100%'}}>
-                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.35)', height: '90%', margin: 10}}>
+                                <div className='col-12 col-md-6' style={{backgroundColor: 'rgba(0,0,0,0.55)', height: '95%', margin: 5}}>
                                     <h1 style={{color:'#cfcfcf', fontSize: '1.35em'}}>PHYDA</h1>
                                     <p style={{color:'#efefef', fontSize: '1.0em', textDecoration: 'none'}}>
                                     This Paleo Hydrodynamics Data Assimilation product (PHYDA) visualization tool maps 2,000 years of reconstructed hydroclimate and associated climate dynamical variables onto a global interface. Using annually or seasonally resolved global reconstructions, two spatiotemporal drought indices, near-surface air temperature, This innovative transformation optimizes exploration of an expansive database into an approachable web-based visualization platform for streamlined presentation and collaboration.
